dd47a9690e9e7429c1a996c57daaa5e42c6ac9b5
Gonzalo Capelari <capelarigonzalo@gmail.com>
2026-09-12 18:16:37 -0300

    agregué paths al header

--- app/componentes/header.tsx
+++ app/componentes/header.tsx
@@ -1,7 +1,20 @@
+"use client"
 import "../styles/header.css"
+import { useRouter } from "next/navigation";
 export default function Header(){
-
-
+const router = useRouter();
+function ira(dir:number){
+switch(dir){
+    case 1: router.push("/");
+    break;
+        case 2: router.push("/teoria/");
+        break;
+                case 3: router.push("/practica/");
+                break;
+                        case 4: router.push("/");
+                        break;
+}
+}
     return(
         <div className="header">
             <section className="burger"><div className="barra"></div>
@@ -10,13 +23,13 @@ export default function Header(){
             <div style={{height: "2px"}}/>
             <div className="barra"></div></section>
             <ul className="listaHeader">
-                <li><button className="botonHEAD">Inicio</button></li>
+                <li><button className="botonHEAD" onClick={() => ira(1)}>Inicio</button></li>
                 <div style={{height: "6px"}}/>
-                <li><button className="botonHEAD">Teoria</button></li>
+                <li><button className="botonHEAD" onClick={() => ira(2)}>Teoria</button></li>
                 <div style={{height: "6px"}}/>
-                <li><button className="botonHEAD">Practica</button></li>
+                <li><button className="botonHEAD " onClick={() => ira(3)}>Practica</button></li>
                 <div style={{height: "6px"}}/>
-                <li><button className="botonHEAD">Sobre mi</button></li>
+                <li><button className="botonHEAD" onClick={() => ira(4)}>Sobre mi</button></li>
                 </ul>
         </div>
     );
